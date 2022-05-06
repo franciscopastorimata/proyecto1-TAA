aa1db720ed154820560bb0b092cc4c2c6c9a60b1
mariajosemillan <mariamillanmeharu@gmail.com>
2022-05-06 20:59:12 -0300

    Add comet scripts

--- src/models/evaluate_models.py
+++ src/models/evaluate_models.py
@@ -1,19 +1,24 @@
+from sklearn import metrics
+from comet_ml import Experiment
 import numpy as np
+import pandas as pd
+from xgboost import XGBClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import ParameterGrid
 from sklearn.model_selection import StratifiedKFold
 
 import sys
+sys.path.append("../../")
 sys.path.append("../../utils")
 from HiggsBosonUtils import AMS
-
+from utils.comet_ import create_experiment, log_experiment, plot_cv_scores
 
 def get_signal_background(y_true, y_pred, weights):
     s = weights[(y_pred==1) & (y_true==1)].sum()
     b = weights[(y_pred==1) & (y_true==0)].sum()
     return s, b
 
-
-def AMSGridSearchCV(model , param_grid, cv, X, y, weights):
+def AMSGridSearchCV(model, param_grid, cv, X, y, weights):
     best_score = 0
     skf = StratifiedKFold(n_splits=3, shuffle=False)
 
@@ -27,7 +32,7 @@ def AMSGridSearchCV(model , param_grid, cv, X, y, weights):
             X_train, X_val = X[train_index], X[val_index]
             y_train, y_val = y[train_index], y[val_index]
             weights_train = weights[train_index]
-            weights_test = weights[val_index]
+            weights_val = weights[val_index]
             
             # ajuste de pesos
             weight_signal_as = weights_train[y_train == 1].sum()
@@ -35,19 +40,51 @@ def AMSGridSearchCV(model , param_grid, cv, X, y, weights):
             weights_train[y_train == 1] *= (weights_signal_bs/weight_signal_as)
             weights_train[y_train == 0] *= (weights_bg_bs/weight_bg_as)
 
-            weight_signal_as = weights_test[y_val == 1].sum()
-            weight_bg_as = weights_test[y_val == 0].sum()
-            weights_test[y_val == 1] *= (weights_signal_bs/weight_signal_as)
-            weights_test[y_val == 0] *= (weights_bg_bs/weight_bg_as)
+            weight_signal_as = weights_val[y_val == 1].sum()
+            weight_bg_as = weights_val[y_val == 0].sum()
+            weights_val[y_val == 1] *= (weights_signal_bs/weight_signal_as)
+            weights_val[y_val == 0] *= (weights_bg_bs/weight_bg_as)
 
-            model.fit(X_train, y_train)
+            model.fit(X_train, y_train, sample_weight=weights_train)
             # score con AMS
             y_pred = model.predict(X_val)
-            s, b = get_signal_background(y_val, y_pred, weights_test)
+            s, b = get_signal_background(y_val, y_pred, weights_val)
             score.append(AMS(s, b))
-            print(score)
+        print(f'{score}, {g}\n\n')
         mean_score = np.mean(score)
         if mean_score > best_score:
             best_scores = score
             best_params = g
-    return best_scores, best_params        
+    return best_scores, best_params
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        print('NO se entreno ningun modelo.')
+    else:
+        INPUT_DATA_PATH = "../../data/output"
+        data_training = pd.read_csv('../../data/input/training.csv')
+        X_train = np.loadtxt(f"{INPUT_DATA_PATH}/X_train.txt")
+        y_train = np.loadtxt(f"{INPUT_DATA_PATH}/y_train.txt")
+        weights = np.array(data_training['Weight'])
+
+        model_to_train = sys.argv[1]
+
+        exp = create_experiment()
+        if model_to_train == 'rf':
+            model = RandomForestClassifier()
+            rf_grid = {'n_estimators': [50],
+                            'min_samples_leaf': [1],
+                            'max_depth': [10],}
+            best_scores, best_params = AMSGridSearchCV(model, param_grid=rf_grid, cv=3, X=X_train, y=y_train, weights=weights)
+            log_experiment(exp, params=rf_grid, metrics=np.mean(best_scores))
+            log_experiment(exp, best_params=best_params)
+            plot_cv_scores(exp, best_scores)
+            print('Se entreno un Random Forest.')
+            print('best_scores: ', np.mean(best_scores))
+            print('best_params: ', best_params)
+        elif model_to_train == 'xgb':
+            model = XGBClassifier()
+            print('Se entrno un XGBoost.')
+        else:
+            print('NO se entrno ningun modelo. Las opciones de modelos a entranr son: rf, xbg')
+
---
+++ utils/comet_.py
@@ -0,0 +1,51 @@
+import os
+from comet_ml import Experiment
+from dotenv import load_dotenv
+import matplotlib.pyplot as plt
+load_dotenv()
+
+# Create an experiment with your api key
+def create_experiment(tags=None):
+    """ Crea el experimento con los tags pasados como variable y lo devuelve."""
+    experiment = Experiment(
+        api_key=os.getenv('COMET_API_KEY'),
+        project_name="general",
+        workspace=os.getenv('COMET_WORKSPACE'),
+        auto_param_logging=False,
+    )
+
+    if tags:
+        for tag in tags:
+            experiment.add_tag(tag)
+
+    return experiment
+
+
+def log_experiment(experiment:Experiment, params=None, metrics=None, best_params=None):
+    """ Logea tanto los parametros como las metricas. """
+    if params!=None:
+        experiment.log_parameters(params)
+    if metrics!=None:
+        experiment.log_metric("AMS", metrics)
+    if best_params!=None:
+        experiment.log_other("best_params", best_params)
+
+    return True
+
+def plot_cv_scores(cmt_exp, scores):
+    '''
+    Entrada:
+        cmt_exp: experimento comet
+        scores: cross-validation AMS scores
+    '''
+    ### Registro de Gráficas ###
+    plt.figure()
+    plt.plot(scores,'*-')
+    plt.xlabel('fold')
+    plt.ylabel('AMS')
+    plt.title('AMS score')
+    plt.grid()
+    
+    cmt_exp.log_figure(figure_name="CV_AMS" ,figure=plt)
+   
+    return
